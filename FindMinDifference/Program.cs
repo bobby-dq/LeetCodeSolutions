d312121eff00e5c9f0df91340f0ed2a9396a61bf
bobby-dq <josh.quilacio@gmail.com>
2020-12-28 13:01:42 -0700

    added some comments in the FindMinDifference solutions

--- FindMinDifference/Program.cs
+++ FindMinDifference/Program.cs
@@ -8,6 +8,7 @@ namespace FindMinDifference
     {
         static void Main(string[] args)
         {
+            // This is the test subject    
             List<string> strings = new List<string>()
             {
                 "05:31","22:08","00:35"
@@ -19,24 +20,27 @@ namespace FindMinDifference
         static int TestMethod(List<string> timePoints)
         {
 
+
             List<int> timePointsInMins = new List<int>(); 
 
+            // Parses each string data in timePoints list (eg. "23:59") into minutes (eg. 1439).
             for (int i = 0; i < timePoints.Count; i++)
             {
                 int hoursToMins = Int32.Parse(timePoints[i].Substring(0,2))*60;
                 int mins = Int32.Parse(timePoints[i].Substring(3));
                 timePointsInMins.Add(hoursToMins+mins);
             }
+
+            // Sorts the items in the timePointsInMins list in ascending order.
             timePointsInMins.Sort();
-            foreach (int i in timePointsInMins)
-            {
-                Console.WriteLine(i);
-            }
 
-            Console.WriteLine("End of parsing and sorting");
-
+            // The initial value of variable "difference"
+            // is the difference betweeen the last item and the first item in the timePointsInMins list
             int difference = (1440 - timePointsInMins[timePoints.Count - 1]) + timePointsInMins[0];
 
+            // The difference between each item and the item after it is compared to variable "difference"
+            // If less than the current value of variable "difference", this will be the new value for the variable "difference"
+            // The difference between the last item and the first item is not included in this loop, since it was already calculated above.
             for (int i = 0; i < timePointsInMins.Count - 1; i++)
             {
                 int differenceCheck = timePointsInMins[i+1] - timePointsInMins[i];
--- README.md
+++ README.md
@@ -11,6 +11,8 @@ This is a repository of my attempt and solutions to various LeetCode Problems. M
 
 - #### 539.Minimum Time Difference
     - Given a list of 24-hour clock time points in "HH:MM" format, return the minimum minutes difference between any two time-points in the list.
+    - Runtime: 104 ms, faster than 76.47% of C# online submissions for Minimum Time Difference.
+    - Memory Usage: 28.5 MB, less than 32.35% of C# online submissions for Minimum Time Difference
     - [Minimum Time Difference LeetCode Link](https://leetcode.com/problems/minimum-time-difference/)
 
 
