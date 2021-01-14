7ada700b29e995f04adf63dcd6ad422ca0a79439
bobby-dq <josh.quilacio@gmail.com>
2021-01-14 00:42:58 -0700

    Initialized a new .net console for #1451 problem.

---
+++ RearrangeWordsInASentence/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace RearrangeWordsInASentence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        public string ArrangeWords(string text)
+        {
+            string[] words = text.Split(' ');
+            string[] rearrangedWords = new string[words.Length]{};
+
+
+        }
+    }
+}
